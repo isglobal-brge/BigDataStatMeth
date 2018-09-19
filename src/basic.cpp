0102a86da78d39953adb5c7b9a5950f993144261
dpelegri <dpelegri@gmail.com>
2018-09-19 18:25:46 +0200

    Nuevas modificaciones
    Primera versión mas modificación de la función que trabaja con matrices Eigen, ahora devuelve un objeto HDF5Matrix en lugar de una matriz numerica

--- src/basic.cpp
+++ src/basic.cpp
@@ -15,45 +15,67 @@ Rcpp::RObject BDtrans_numeric (const Rcpp::RObject & x)
   auto dmtype = beachmat::find_sexp_type(x);
   
   if ( dmtype == INTSXP ) {
+    
     auto dmat = beachmat::create_integer_matrix(x);
     const size_t ncols = dmat->get_ncol();
     const size_t nrows = dmat->get_nrow();
     
-    size_t ccol;//, crow;
-    Eigen::MatrixXi proves(nrows,ncols);
-    
+    Eigen::MatrixXi mateigen(nrows,ncols);
     Rcpp::IntegerVector output(dmat->get_nrow());
     
-    for (ccol=0; ccol<ncols; ++ccol) {
+    for (size_t ccol=0; ccol<ncols; ++ccol) {
       dmat->get_col(ccol, output.begin());
-      proves.col(ccol) = Rcpp::as<Eigen::VectorXi >(output);
+      mateigen.col(ccol) = Rcpp::as<Eigen::VectorXi >(output);
     }
     
-    proves.transposeInPlace() ;
+    mateigen.transposeInPlace();
     
-    return wrap(proves);
+    beachmat::output_param oparam(dmat->get_matrix_type(), FALSE, TRUE);  
+    auto out_dmat = beachmat::create_integer_output(ncols, nrows, oparam);
+    
+    Rcpp::IntegerMatrix matint = wrap(mateigen);
+    Rcpp::IntegerVector vint;
+    
+    for (size_t nrow=0; nrow <ncols; ++nrow) {
+      vint = matint.row(nrow);
+      out_dmat ->set_row(nrow, vint.begin());
+    }
+    
+    return(out_dmat->yield());
     
   } else if (dmtype==REALSXP) {
+    
     auto dmat = beachmat::create_numeric_matrix(x);
     const size_t ncols = dmat->get_ncol();
     const size_t nrows = dmat->get_nrow();
     
-    size_t ccol;//, crow;
-    Eigen::MatrixXd proves(nrows,ncols);
-    
+    Eigen::MatrixXd mateigen(nrows,ncols);
     Rcpp::NumericVector output(dmat->get_nrow());
     
-    for (ccol=0; ccol<ncols; ++ccol) {
+    for (size_t ccol=0; ccol<ncols; ++ccol) {
       dmat->get_col(ccol, output.begin());
-      proves.col(ccol) = Rcpp::as<Eigen::VectorXd >(output);
+      mateigen.col(ccol) = Rcpp::as<Eigen::VectorXd >(output);
     }
     
-    proves.transposeInPlace() ;
+    mateigen.transposeInPlace() ;
     
-    return wrap(proves);
+    beachmat::output_param oparam(dmat->get_matrix_type(), FALSE, TRUE);  
+    auto out_dmat = beachmat::create_integer_output(ncols, nrows, oparam);
+    
+    Rcpp::NumericMatrix matd = wrap(mateigen);
+    Rcpp::NumericVector vd;
+    
+    for (size_t nrow=0; nrow <ncols; ++nrow) {
+      vd = matd.row(nrow);
+      out_dmat ->set_row(nrow, vd.begin());
+    }
+    
+    return(out_dmat->yield());
+    
   } else {
     throw std::runtime_error("unacceptable matrix type");
   }
+  
   
 }
 
@@ -138,13 +160,42 @@ Rcpp::RObject BDtrans_hdf5(const Rcpp::RObject & x)
 library(Rcpp)
 library(DelayedArray)
 library("microbenchmark")
-  
-  blah <- matrix(runif(10000), ncol=200, nrow=50)
-  X <- DelayedArray(blah)
 
-  results <- microbenchmark(transR = t(X),
-                            transCpp = BDtrans_numeric(X),
-                            transCpp = BDtrans_hdf5(X),
+
+
+# PROVES MATRIU ENTERS
+  val <- matrix(rep(1:200, 1:200), ncol = 100)
+  da_mat <- DelayedArray(seed = val) ; da_mat
+
+  results <- microbenchmark(transR <-  t(da_mat),
+                            transCppEigen <-  BDtrans_numeric(da_mat),
+                            transCpp <-  BDtrans_hdf5(da_mat),
                             times = 50L)
 
+  print(summary(results)[, c(1:7)],digits=1)
+  
+  #all.equal(transR,transCppEigen)
+  #all.equal(transCpp,transCppEigen)
+  
+
+# PROVES MATRIU REALS
+  provesdelay <- matrix(runif(100000), ncol=200, nrow=500)
+  X <- DelayedArray(blah)
+
+  results <- microbenchmark(transR <- t(X),
+                            transCppEigen <- BDtrans_numeric(X),
+                            transCpp <- BDtrans_hdf5(X),
+                            times = 50L)
+  print(summary(results)[, c(1:7)],digits=1)
+  
+# PROVES MATRIU CARACTERS
+  delaystring <- matrix(sample(LETTERS, 10000, TRUE), ncol=200, nrow=50)
+  X <- DelayedArray(delaystring)
+  
+  
+  results <- microbenchmark(transR <- t(X),
+                            #transCppEigen <- BDtrans_numeric(X),
+                            transCpp <- BDtrans_hdf5(X),
+                            times = 50L)
+  print(summary(results)[, c(1:7)],digits=1)
 */
--- tests/script.R
+++ tests/script.R
@@ -8,15 +8,38 @@ identical(ans1, ans2)
 max(ans1 - ans2)
 
 
-
+library(Rcpp)
 library(DelayedArray)
 library("microbenchmark")
 
-blah <- matrix(runif(10000), ncol=200, nrow=50)
+# PROVES MATRIU ENTERS
+val <- matrix(rep(1:200, 1:200), ncol = 100)
+da_mat <- DelayedArray(seed = val) ; da_mat
+
+results <- microbenchmark(transR <-  t(da_mat),
+                          transCppEigen <-  BDtrans_numeric(da_mat),
+                          transCpp <-  BDtrans_hdf5(da_mat),
+                          times = 50L)
+
+print(summary(results)[, c(1:7)],digits=1)
+
+# PROVES MATRIU REALS
+provesdelay <- matrix(runif(100000), ncol=200, nrow=500)
 X <- DelayedArray(blah)
 
-# 
-results <- microbenchmark(transR = t(X),
-                          transCpp = BDtrans_numeric(X),
-                          transCpp = BDtrans_hdf5(X),
+results <- microbenchmark(transR <- t(X),
+                          transCppEigen <- BDtrans_numeric(X),
+                          transCpp <- BDtrans_hdf5(X),
                           times = 50L)
+print(summary(results)[, c(1:7)],digits=1)
+
+# PROVES MATRIU CARACTERS
+delaystring <- matrix(sample(LETTERS, 10000, TRUE), ncol=200, nrow=50)
+X <- DelayedArray(delaystring)
+
+
+results <- microbenchmark(transR <- t(X),
+                          #transCppEigen <- BDtrans_numeric(X),
+                          transCpp <- BDtrans_hdf5(X),
+                          times = 50L)
+print(summary(results)[, c(1:7)],digits=1)
