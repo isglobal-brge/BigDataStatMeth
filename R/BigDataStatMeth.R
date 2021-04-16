09b61936b6fbc00aa489ad3593f24cafe0ab54e3
dpelegri <dpelegri@gmail.com>
2021-04-16 13:00:05 +0200

    Remove OLD BigDataStatMeth

--- R/BigDataStatMeth.R
+++
@@ -1,9 +0,0 @@
-#' BigDataStatMEth package documentation
-#' @name BigDataStatMeth
-#' @useDynLib BigDataStatMeth
-#' @import RcppEigen
-#' @importFrom Rcpp evalCpp
-#' @importFrom Rcpp sourceCpp
-#' @importFrom RcppParallel RcppParallelLibs
-#' @exportPattern("^[[:alpha:]]+")
-NULL
--- README.md
+++
@@ -1,3 +0,0 @@
-# BigDataStatMeth
-
-**NOTE:** This package is defunct ... It has been moved to [BDSM](https://github.com/isglobal-brge/BDSM)
