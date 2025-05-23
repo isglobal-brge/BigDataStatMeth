8d85bfe76f32acce2f2dd545d261f2cf136ed826
dpelegri <43083225+dpelegri@users.noreply.github.com>
2025-05-23 18:14:31 +0200

    more about documenting functions

--- R/ImportData_hdf5.R
+++ R/ImportData_hdf5.R
@@ -1,37 +1,58 @@
-#' Import data from url or a file
+#' Import data from URL or file to HDF5 format
 #' 
-#' This function download data from an url and decompress data (if needed), 
-#' then imports the file to hdf5 data file
+#' This function downloads data from a URL (if URL is provided) and decompresses it 
+#' if needed, then imports the data into an HDF5 file. It supports both local files 
+#' and remote URLs as input sources.
 #' 
-#' @export
-#' 
-#' @param inFile string file name or url with data to import
-#' @param destFile file name and path to store imported data
-#' @param destGroup group name to store the dataset
-#' @param destDataset dataset name to store the input file in hdf5
-#' @param header (optional) either a logical value indicating whether the column 
-#' names of x are to be written along with x, or a character vector of column 
-#' names to be written. See the section on ‘CSV files’ for the meaning of 
-#' col.names = NA.
-#' @param rownames (optional) either a logical value indicating whether the row 
-#' names of x are to be written along with x, or a character vector of row names 
-#' to be written.
-#' @param overwrite (optional) either a logical value indicating whether the 
-#' output file can be overwritten or not.
-#' @param overwriteFile logical (optional), CAUTION, if TRUE, file will be 
-#' overwritten with imported dataset, by default `fileoverwrite = FALSE` to avoid
-#' file overwritting.
-#' @param sep (optional), by default = "\\t". The field separator string. Values 
-#' within each row of x are separated by this string.
-#' @param paral, (optional, default = TRUE) if paral = TRUE performs parallel 
-#' computation else performs seria computation
-#' @param threads (optional) only if bparal = true, number of concurrent 
-#' threads in parallelization if threads is null then threads =  maximum 
-#' number of threads available
+#' @param inFile Character string specifying either a local file path or URL containing 
+#'        the data to import
+#' @param destFile Character string specifying the file name and path where the HDF5 
+#'        file will be stored
+#' @param destGroup Character string specifying the group name within the HDF5 file 
+#'        where the dataset will be stored
+#' @param destDataset Character string specifying the name for the dataset within 
+#'        the HDF5 file
+#' @param header Logical or character vector. If TRUE, the first row contains column 
+#'        names. If a character vector, use these as column names. Default is TRUE.
+#' @param rownames Logical or character vector. If TRUE, first column contains row 
+#'        names. If a character vector, use these as row names. Default is FALSE.
+#' @param overwrite Logical indicating if existing datasets should be overwritten. 
+#'        Default is FALSE.
+#' @param overwriteFile Logical indicating if the entire HDF5 file should be 
+#'        overwritten if it exists. CAUTION: This will delete all existing data. 
+#'        Default is FALSE.
+#' @param sep Character string specifying the field separator in the input file. 
+#'        Default is "\\t" (tab).
+#' @param paral Logical indicating whether to use parallel computation. Default is TRUE.
+#' @param threads Integer specifying the number of threads to use for parallel 
+#'        computation. Only used if paral=TRUE. If NULL, uses maximum available threads.
+#'
+#' @return No return value. The function writes the data directly to the specified 
+#'         HDF5 file.
+#'
 #' @examples
-#'    print ("Example in vignette")
+#' \dontrun{
+#' # Import from local file
+#' bdImportData_hdf5(
+#'   inFile = "data.txt",
+#'   destFile = "output.h5",
+#'   destGroup = "mydata",
+#'   destDataset = "matrix1",
+#'   header = TRUE,
+#'   sep = "\t"
+#' )
+#' 
+#' # Import from URL
+#' bdImportData_hdf5(
+#'   inFile = "https://example.com/data.csv",
+#'   destFile = "output.h5",
+#'   destGroup = "downloaded",
+#'   destDataset = "remote_data",
+#'   sep = ","
+#' )
+#' }
 #'    
-#' @return none value returned, data are stored in a dataset inside an hdf5 data file.
+#' @export
 bdImportData_hdf5 <- function( inFile, destFile, destGroup, destDataset, 
                                header = TRUE, rownames = FALSE, 
                                overwrite = FALSE, overwriteFile = FALSE, 
